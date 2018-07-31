4b1c758c3284b40f93f6a3c320ddeb0ac39ea1d2
Dwina Solihin <dwinasolihin@gmail.com>
2018-07-30 19:19:30 -0700

    Getting two charts on the page

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ google.charts.load('current', {'packages':['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawAllocationChart);
+
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
@@ -33,3 +35,25 @@ google.charts.setOnLoadCallback(drawChart);
     var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
     chart.draw(data, options);
   }
+//-----------------------------------------------------------------------------------------------
+  function drawAllocationChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Score Range');
+    data.addColumn('number', 'Games');
+    data.addRows([
+      ['0-100', 2],
+      ['100-200', 5],
+      ['200-300', 3]
+   ]);
+
+    // Set chart options
+    var options = {'title':'Bowling Score Incidences',
+      'width':400,
+      'height':300};
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('allocation_chart'));
+      chart.draw(data, options);
+    }
